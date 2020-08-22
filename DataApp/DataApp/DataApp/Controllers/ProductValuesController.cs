182df650a978aeba7220018001348deabb58541d
Влад Мисниченко <qwerty326159487@mail.ru>
2020-08-22 20:41:34 +0300

    Added product values controller

---
+++ DataApp/DataApp/DataApp/Controllers/ProductValuesController.cs
@@ -0,0 +1,22 @@
+﻿using DataApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products = new Product[] 
+            {
+                new Product { Name = "Ball", Category = "Sport", Price = 200 },
+                new Product { Name = "Apple", Category = "Fruit", Price = 1 }
+            };
+
+            return Ok(products);
+        }
+    }
+}
